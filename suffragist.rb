3134d2241272833f46bd5b258b90c0d4e06d63ab
Najmi Syahir <najmi.syahir@gmail.com>
2017-12-12 17:00:01 +0800

    Added routes folder and change how to start web server using port 8080

--- suffragist.rb
+++ suffragist.rb
@@ -1,33 +1,12 @@
-require 'sinatra'
-require 'yaml/store'
+require 'sinatra/base'
+require "bundler/setup"
 
-Choices = {
-  'HAM' => 'Hamburger',
-  'PIZ' => 'Pizza',
-  'CUR' => 'Curry',
-  'NOO' => 'Noodles',
+class Suffragist < Sinatra::Application
+  Dir[File.join(File.dirname(__FILE__), "routes", "*.rb")].each { |lib| require lib }
+end
+
+server_options = {
+  :Port => "8080",
+  :BindAddress => "0.0.0.0",
 }
-
-get '/' do
-	@title = 'Welcome to the Suffragist!'
-  	erb :index
-end
-
-post '/cast' do
-  @title = 'Thanks for casting your vote!'
-  @vote  = params['vote']
-  @store = YAML::Store.new 'votes.yml'
-  @store.transaction do
-    @store['votes'] ||= {}
-    @store['votes'][@vote] ||= 0
-    @store['votes'][@vote] += 1
-  end
-  erb :cast
-end
-
-get '/results' do
-  @title = 'Results so far:'
-  @store = YAML::Store.new 'votes.yml'
-  @votes = @store.transaction { @store['votes'] }
-  erb :results
-end
+Rack::Handler::WEBrick.run Suffragist, server_options
